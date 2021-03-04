0f778ac3ca907af9641479a0586ce494b0e8ce6a
Akash Negi <akash8900@gmail.com>
2021-03-04 20:49:28 +0530

    Fixed Links Bug

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -51,13 +51,13 @@ function Header() {
           <a href='https://github.com/NegiAkash890'>
             <i className='fa fa-github fa-2x'></i>
           </a>
-          <a href='www.instagram.com/akash_negi89/'>
+          <a href='https://www.instagram.com/akash_negi89/'>
               <i className='fa fa-instagram fa-2x'></i>
           </a>
            <a href='mailto:akash8900@gmail.com' >
               <i className='fa fa-envelope fa-2x'></i>
           </a>
-           <a href='www.linkedin.com/in/akash-negi-8900'>
+           <a href='https://www.linkedin.com/in/akash-negi-8900'>
               <i className='fa fa-linkedin fa-2x'></i>
           </a>
         </div>
